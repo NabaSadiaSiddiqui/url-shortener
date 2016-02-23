dd3477d4d5977976a51083a01615c69dbc172274
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2016-02-22 23:39:11 -0500

    Update controller * Add mapping for index page * Update mapping to url-shortner html page

--- src/main/java/com/naba/url/shortner/UrlShortnerController.java
+++ src/main/java/com/naba/url/shortner/UrlShortnerController.java
@@ -2,15 +2,20 @@ package com.naba.url.shortner;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UrlShortnerController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @RequestMapping("/")
+    public String landingPage() {
+        return "index";
+    }
+
+    @RequestMapping("/url-shortner/{url}")
+    public String urlShortner(@PathVariable String url, Model model) {
+        model.addAttribute("name", url);
+        return "gretting";
     }
 }
