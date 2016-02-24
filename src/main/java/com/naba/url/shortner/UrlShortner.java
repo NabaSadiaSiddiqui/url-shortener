0848c0b828385bafa843efa70bc715e3b9ca036b
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2016-02-23 21:28:03 -0500

    Add model to capture form data from home page

---
+++ src/main/java/com/naba/url/shortner/UrlShortner.java
@@ -0,0 +1,13 @@
+package com.naba.url.shortner;
+
+public class UrlShortner {
+    private String originalUrl;
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String url) {
+        this.originalUrl = url;
+    }
+}
