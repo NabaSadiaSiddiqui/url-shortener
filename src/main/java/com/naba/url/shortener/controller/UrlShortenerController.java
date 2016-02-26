07f9c38076c92b539dbaf6583637db075d15b6d8
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2016-02-25 21:53:26 -0500

    Add logic to redirect to original url

--- src/main/java/com/naba/url/shortener/controller/UrlShortenerController.java
+++ src/main/java/com/naba/url/shortener/controller/UrlShortenerController.java
@@ -3,12 +3,17 @@ package com.naba.url.shortener.controller;
 import com.naba.url.shortener.model.UrlShortener;
 import com.naba.url.shortener.model.UrlState;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.net.URI;
 
 @Controller
 public class UrlShortenerController {
@@ -33,12 +38,10 @@ public class UrlShortenerController {
     }
 
     @RequestMapping(value = "/better-than-bitly/{url}", method = RequestMethod.GET)
-    public String redirect(@PathVariable String url, Model model) throws Exception {
-        String originalUrl = urlShortener.getOriginalUrl(url);
-        System.out.println(originalUrl);
-        UrlState urlState = new UrlState();
-        urlState.setOriginalUrl(originalUrl);
-        model.addAttribute("urlShortener", urlState);
-        return "redirect";
+    public ResponseEntity<String> redirect(@PathVariable String url) throws Exception {
+        URI originalUrl = new URI(urlShortener.getOriginalUrl(url));
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(originalUrl);
+        return new ResponseEntity<String>(httpHeaders, HttpStatus.SEE_OTHER);
     }
 }
