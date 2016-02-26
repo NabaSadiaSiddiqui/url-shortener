26ab6ac1d052f334edda2352b745258ac142b02c
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2016-02-25 20:56:43 -0500

    Use Lombok for POJOs

--- src/main/java/com/naba/url/shortner/UrlShortner.java
+++ src/main/java/com/naba/url/shortner/UrlShortner.java
@@ -1,23 +1,9 @@
 package com.naba.url.shortner;
 
+import lombok.Data;
+
+@Data
 public class UrlShortner {
     private String originalUrl;
-
     private String shortnedUrl;
-
-    public String getOriginalUrl() {
-        return originalUrl;
-    }
-
-    public String getShortnedUrl() {
-        return shortnedUrl;
-    }
-
-    public void setOriginalUrl(String url) {
-        this.originalUrl = url;
-    }
-
-    public void setShortnedUrl(String url) {
-        this.shortnedUrl = url;
-    }
 }
